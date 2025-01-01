7492f92b1c8099d518b8ad8d671a84495705e3fe
wunna11 <wana11391@gmail.com>
2025-01-01 22:17:21 +0630

    set up liveblocks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
-  title: 'LiveDocs',
-  description: 'Your go-to collaborative editor',
-}
+  title: "LiveDocs",
+  description: "Your go-to collaborative editor",
+};
 
 export default function RootLayout({
   children,
@@ -22,24 +23,24 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
-      appearance={{ 
+      appearance={{
         baseTheme: dark,
         variables: {
-          colorPrimary: '#3371FF',
-          fontSize: '16px'
-        }
-       }}
+          colorPrimary: "#3371FF",
+          fontSize: "16px",
+        },
+      }}
     >
-    <html lang="en">
-      <body
-       className={cn(
-        "min-h-screen font-sans antialiased",
-        fontSans.variable
-      )}
-      >
-        {children}
-      </body>
+      <html lang="en">
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>{children}</Provider>
+        </body>
       </html>
-      </ClerkProvider>
+    </ClerkProvider>
   );
 }
---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,31 @@
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react";
+import React from "react";
+import Loader from "./Loader";
+import Header from "./Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Editor } from "./editor/Editor";
+
+const CollaborativeRoom = () => {
+  return (
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<Loader />}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">This is fake document</p>
+            </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
